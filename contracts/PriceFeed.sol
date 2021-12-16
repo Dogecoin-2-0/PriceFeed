c13be6eebc8c5222aed912c035498e133cb3b50e
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-16 19:16:32 +0100

    update price function

--- contracts/PriceFeed.sol
+++ contracts/PriceFeed.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
 
 import '@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol';
+import '@chainlink/contracts/src/v0.8/Denominations.sol';
 import '@openzeppelin/contracts/utils/math/SafeCast.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
@@ -14,12 +15,8 @@ contract PriceFeed {
     _feed = FeedRegistryInterface(_feedRegistry);
   }
 
-  function getPrice(address _base, address _quote)
-    external
-    view
-    returns (uint256)
-  {
-    (, int256 price, , , ) = _feed.latestRoundData(_base, _quote);
+  function getPriceUSD(address _base) external view returns (uint256) {
+    (, int256 price, , , ) = _feed.latestRoundData(_base, Denominations.USD);
     return price.toUint256();
   }
 }
